62e5cd8ea3ec2258848cc27fb17aa64d4d5bdfa8
armadillo2 <plpbgu@post.bgu.ac.il>
2021-10-18 12:41:59 +0300

    armadillo

--- wrapper_yolo4-tiny.py
+++ wrapper_yolo4-tiny.py
@@ -3,24 +3,23 @@
 import rospy
 import cv2
 import numpy as np
-from std_msgs.msg import String, Int16 
 from sensor_msgs.msg import CompressedImage
-import darknet_images
 import darknet
-import time
-import argparse 
+import argparse
 from pathlib import Path
 import os
 from obj_detector.msg import Detection_msg
 
-#from cv_bridge import CvBridge, CvBridgeError
-
-#---------------------- Discription ----------------------\
+# ---------------------- Discription ----------------------\
 # Subscribe to camera topic and run Yolo4-tiny to detect items. Then
 # the node publish two topics:
 # image with boundaries box
-# detection massage with the field: [header, class id, score, pose [x_center, y_center, size_x, size_y]] 
-# Yolo is changing the image resolution, therefore there is transformation back to armdillo camera resolution.
+# detection massage with the field: [header, class id, score, pose [x_center, y_center, size_x, size_y]]
+# Yolo is changing the image resolution, therefore there is transformation
+# back to armdillo camera resolution.
+# The yolo parameters are define in parser function, the user can define
+# them while excuting this script. Another way to change them is by
+# change the defult value at the parser function.
 
 
 global img, count, new_shape
@@ -28,6 +27,8 @@ global img, count, new_shape
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+
+
 def parser():
     parser = argparse.ArgumentParser(description="YOLO Object Detection")
     parser.add_argument("--camera_topic", default='/kinect2/qhd/image_color_rect/compressed', type=str,
@@ -52,9 +53,11 @@ def parser():
                         help="remove detections with lower confidence")
     return parser.parse_args()
 
+
 def img_lisner(msg):
     global img
     img = msg
+
 
 def image_detection(image, network, class_names, class_colors, thresh):
     # Darknet doesn't accept numpy images.
@@ -72,13 +75,15 @@ def image_detection(image, network, class_names, class_colors, thresh):
     image = darknet.draw_boxes(detections, image_resized, class_colors)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
 
-def video_prossecing(network,class_names, class_colors):
+
+def video_prossecing(network, class_names, class_colors):
     global img, new_shape
     img_cv2 = np.fromstring(img.data, np.uint8)
     frame = cv2.imdecode(img_cv2, cv2.IMREAD_COLOR)
     new_shape = frame.shape
     image, detections = image_detection(frame, network, class_names, class_colors, args.thresh)
     return image, detections
+
 
 def createCompresseImage(cv2_img):
     msg = CompressedImage()
@@ -87,13 +92,14 @@ def createCompresseImage(cv2_img):
     msg.data = np.array(cv2.imencode('.jpg', cv2_img)[1]).tostring()
     return msg
 
+
 def detection_publish(detections, publisher):
     for detection in detections:
         obj_msg = Detection_msg()
         obj_msg.header.stamp = rospy.Time.now()
         obj_msg.header.frame_id = "kinect2_link"
         obj_msg.class_id = detection[0]
-        obj_msg.score =  float(detection[1])
+        obj_msg.score = float(detection[1])
         obj_msg.pose.x_center = detection[2][0]
         obj_msg.pose.y_center = detection[2][1]
         obj_msg.pose.size_x = detection[2][2]
@@ -102,11 +108,11 @@ def detection_publish(detections, publisher):
 
 
 def detection_tf(old_shape, new_shape, detections):
-    # Transformation of detections from Yolo resolution to armadillo. 
+    # Transformation of detections from Yolo resolution to armadillo.
     height_factor = new_shape[0] / old_shape[0]
     width_factor = new_shape[1] / old_shape[1]
     detect_tf = lambda detect: [detect[2][0]*width_factor, detect[2][1]*height_factor,
-                                detect[2][2]*width_factor, detect[2][3]*height_factor] 
+                                detect[2][2]*width_factor, detect[2][3]*height_factor]
     for i in range(len(detections)):
         detections[i][2] = detect_tf(detections[i])
     return detections
@@ -126,7 +132,7 @@ def print_img(detections):
 
 
 def tuple2list(t):
-    return list(map(tuple2list,t)) if isinstance(t, (tuple, list)) else t
+    return list(map(tuple2list, t)) if isinstance(t, (tuple, list)) else t
 
 
 def main(args):
@@ -134,17 +140,21 @@ def main(args):
     print(os.path.exists(args.weights))
     if not os.path.exists(args.weights):
         print("Invalid weight path {}".format(os.path.abspath(args.weights)))
+        download_link = "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights"
+        git_hub = "https://github.com/AlexeyAB/darknet"
         print("1. download weights for tiny-4 from this link: \n"
-              "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights \n\n"
-              "2. Allocate the weights in {}/data/???.weights \n\n"
-              "You can also visit darknet git for more instructions: https://github.com/AlexeyAB/darknet".format(str(Path(__file__).parent)))
+              "{} \n\n 2. Allocate the weights in {}/data/???.weights \n\n You can also visit darknet"
+              " git for more instructions: {}".format(download_link, str(Path(__file__).parent), git_hub))
         return
-    network, class_names, class_colors = darknet.load_network(args.config_file, args.data_file,
-                                            args.weights,batch_size=args.batch_size)
+    network, class_names, class_colors = darknet.load_network(args.config_file,
+                                                              args.data_file,
+                                                              args.weights,
+                                                              batch_size=args.batch_size)
 
     rate = rospy.Rate(args.frame_rate)
-    pub = rospy.Publisher('/yolo4_result/compressed',CompressedImage,queue_size=1)
-    detection_publisher = rospy.Publisher('/yolo4_result/detections', Detection_msg, queue_size= 10)
+    pub = rospy.Publisher('/yolo4_result/compressed', CompressedImage, queue_size=1)
+    detection_publisher = rospy.Publisher('/yolo4_result/detections',
+                                          Detection_msg, queue_size=10)
     rospy.wait_for_message(args.camera_topic, CompressedImage)
 
     while not rospy.is_shutdown():
@@ -160,6 +170,6 @@ def main(args):
 
 if __name__ == "__main__":
     args = parser()
-    rospy.init_node("wrapper_yolo4_tiny",anonymous=True)
+    rospy.init_node("wrapper_yolo4_tiny", anonymous=True)
     rospy.Subscriber(args.camera_topic, CompressedImage, img_lisner, queue_size=1)
     main(args)
